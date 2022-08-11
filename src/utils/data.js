6e040563c62c7b9f34f2db1e9145c3bc81d21e48
Achmad Rif'an <achmadrifanlearn@gmail.com>
2022-08-11 18:32:56 +0700

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Achmad Rifan',
+            tag: 'withrifan',
+            imageUrl: '/images/user1.jpg',
+        },
+        {
+            id: 2,
+            name: 'Achmad Zamzami',
+            tag: 'zamzami',
+            imageUrl: '/images/user2.jpg',
+        },
+        {
+            id: 3,
+            name: 'Achmad Idris',
+            tag: 'aidriss',
+            imageUrl: '/images/user3.jpg',
+        },
+        {
+            id: 4,
+            name: 'Achmad Nur',
+            tag: 'nuurrr',
+            imageUrl: '/images/user4.jpg',
+        },
+    ];
+}
+
+export { getData };
